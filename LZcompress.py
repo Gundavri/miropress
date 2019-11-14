560b928e83a827c6ce98fff7dbdc792a368fbe60
Gundavri <m.mirianashvili99@gmail.com>
2019-11-14 15:54:21 +0400

    First Commit. Both compression and decompression work

---
+++ LZcompress.py
@@ -0,0 +1,112 @@
+from sys import argv
+import os, time
+
+start_time = time.time()
+
+def writeEliasCode(size):
+    binarySize = bin(size)[2:]
+    eliasPrefixSize = len(binarySize) - 1
+    for i in range(eliasPrefixSize):
+        binarySize = '0' + binarySize
+    return binarySize
+
+def readAndWriteFile(fr, num):
+    res = ''
+    for i in range(num):
+        single_byte = fr.read(1)
+        if  not single_byte:
+            break
+        res += writeInBits(ord(single_byte))
+    return res
+
+
+def writeInBits(single_byte):
+    result = ''
+    for i in range(0, 8):
+        single_bit = single_byte << i
+        single_bit = single_bit & 128
+        if  single_bit > 0:
+            result += '1'
+        else:
+            result += '0'
+    return result
+
+
+def findBiggestPref(lex):
+    global longestKeySize
+    for i in range(longestKeySize, 0, -1):
+        pref = codeToCompress[0:i]
+        if pref in lex:
+            return pref
+    return codeToCompress
+
+def is2sPower(num):
+    while num > 1 :
+        num /= 2
+    return num == 1
+
+def makeLexLonger(lex, currLen):
+    for key in lex:
+        if len(lex[key]) == currLen:
+            lex[key] = '0' + lex[key]
+
+def changeLexicon(biggestPref, lex):
+    global longestKeySize
+    currLen = len(lex[biggestPref])
+    lex[biggestPref+'0'] = lex[biggestPref]
+    del lex[biggestPref]
+    newVal = bin(len(lex))[2:]
+    lex[biggestPref+'1'] = newVal
+
+    newKeyLen = len(biggestPref) + 1
+    if longestKeySize < newKeyLen:
+        longestKeySize = newKeyLen
+
+    if is2sPower(len(lex)-1):
+        makeLexLonger(lex, currLen)
+    
+def writeInFile(code):
+    while len(code) > 0:
+        temp = code[0:8]
+        code = code[8:]
+        temp = int(temp, 2)
+        fileToWrite.write(temp.to_bytes(1, "little"))
+
+size = os.path.getsize(argv[1])
+
+codeToCompress = ''
+longestKeySize = 1
+codeToWrite = writeEliasCode(size)
+
+lexicon = {
+    '0': '0',
+    '1': '1'
+}
+
+fileToRead = open(argv[1], 'rb')
+fileToWrite = open(argv[2], 'wb')
+
+codeToCompress = readAndWriteFile(fileToRead, 1)
+
+while len(codeToCompress) > 0:
+    if longestKeySize+1 > len(codeToCompress): 
+        codeToCompress += readAndWriteFile(fileToRead, (longestKeySize//8)+1)
+    biggestPref = findBiggestPref(lexicon)
+    while biggestPref not in lexicon:
+        biggestPref += '0'
+    codeToWrite += lexicon[biggestPref]
+    codeToCutSize = len(codeToWrite)//8*8
+    writeInFile(codeToWrite[0:codeToCutSize])
+    codeToWrite = codeToWrite[codeToCutSize:]
+    changeLexicon(biggestPref, lexicon)
+    codeToCompress = codeToCompress[len(biggestPref):]
+
+codeToWrite += '1'
+while len(codeToWrite)%8 != 0:
+    codeToWrite += '0'
+
+writeInFile(codeToWrite)
+fileToRead.close()
+fileToWrite.close()
+
+print(str(time.time()-start_time) + ' --- Compress Time')
---
+++ LZdecompress.py
@@ -0,0 +1,110 @@
+from sys import argv
+import time
+
+start_time = time.time()
+
+def is2sPower(num):
+    while num > 1 :
+        num /= 2
+    return num == 1
+
+def makeLexLonger(lex, currLen):
+    first = lex.copy()
+    for key in first:
+        if len(key) == currLen:
+            lex['0'+key] = lex[key]
+            del lex[key]
+
+def readAndWriteFile(fr, num):
+    res = ''
+    for i in range(num):
+        single_byte = fr.read(1)
+        if  not single_byte:
+            break
+        res += writeInBits(ord(single_byte))
+    return res
+
+def writeInBits(single_byte):
+    result = ''
+    for i in range(0, 8):
+        single_bit = single_byte << i
+        single_bit = single_bit & 128
+        if  single_bit > 0:
+            result += '1'
+        else:
+            result += '0'
+    return result
+
+def bufferEliasCode(fr):
+    global codeToDecompress
+    counter = 0
+    while True:
+        codeToDecompress += readAndWriteFile(fr, 1)
+        counter += 1
+        if int(codeToDecompress) > 0:
+            break
+    for i in range(counter):
+        codeToDecompress += readAndWriteFile(fr, 1)
+
+def findFileSize(fr):
+    global codeToDecompress
+    global fileToWriteSize
+    counter = 0
+    for i in codeToDecompress:
+        if i == '0':
+            counter += 1
+        else:
+            break
+    
+    binaryString = codeToDecompress[counter:2*counter+1]
+    codeToDecompress = codeToDecompress[2*counter+1:]
+    fileToWriteSize = int(binaryString, 2)
+    codeToDecompress += readAndWriteFile(fr, 1)
+
+def writeInFile(code):
+    while len(code) > 0:
+        temp = code[0:8]
+        code = code[8:]
+        temp = int(temp, 2)
+        fileToWrite.write(temp.to_bytes(1, "little"))
+
+codeToDecompress = ''
+codeToWrite = ''
+fileToWriteSize = 0
+keySize = 1
+
+
+lexicon = {
+    '0': '0',
+    '1': '1'
+}
+
+fileToRead = open(argv[1], 'rb')
+fileToWrite = open(argv[2], 'wb')
+bufferEliasCode(fileToRead)
+findFileSize(fileToRead)
+
+while len(codeToDecompress)>0:
+    if keySize+1 > len(codeToDecompress):
+        codeToDecompress += readAndWriteFile(fileToRead, keySize//8 + 1)
+    key = codeToDecompress[0:keySize]
+    codeToDecompress = codeToDecompress[keySize:]
+    val = ''
+    try:
+        val = lexicon[key]
+    except:
+        break    
+    codeToWrite += val
+    codeToCutSize = len(codeToWrite)//8*8
+    writeInFile(codeToWrite[0:codeToCutSize])
+    codeToWrite = codeToWrite[codeToCutSize:]
+    lexicon[key] = val + '0'
+    lexicon[bin(len(lexicon))[2:]] = val + '1'
+    if is2sPower(len(lexicon)-1) and len(lexicon) != 2:
+        makeLexLonger(lexicon, keySize)
+        keySize += 1
+
+fileToRead.close()
+fileToWrite.close()
+
+print(str(time.time()-start_time) + ' --- Decompress Time')
